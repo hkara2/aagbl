4bc97c7c4481acc50bb2ceffeca7d4fed18b1004
hkaradimas <hkaradimas@ch-sudessonne.fr>
2026-05-07 20:36:26 +0200

    added methods to get names, fixed javadocs

--- src/main/java/org/hkmi2/aagbl/AsciiArtGridBagLayout.java
+++ src/main/java/org/hkmi2/aagbl/AsciiArtGridBagLayout.java
@@ -22,9 +22,13 @@ import java.util.Map;
 public class AsciiArtGridBagLayout
 extends GridBagLayout
 {
+  /** The builder */
   GridBagLayoutBuilder gblb;
+  /** All the components by name */
   HashMap<String, Component> componentsByName = new HashMap<>();
+  /** All the constraints by name */
   HashMap<String, GridBagConstraints> constraintsByName = new HashMap<String, GridBagConstraints>();
+  /** All the CRects */
   List<CRect> crects;
   
   /**
@@ -32,6 +36,7 @@ extends GridBagLayout
    */
   private static final long serialVersionUID = 1L;
   
+  /** The asciiArt drawing */
   String asciiArt;
 
   /**
@@ -53,19 +58,36 @@ extends GridBagLayout
     return hm;
   }
   
+  /**
+   * Constructor with a drawing
+   * @param asciiArt The Ascii art drawing to use
+   * @throws LayoutParseException If something goes wrong during layout
+   */
   public AsciiArtGridBagLayout(String asciiArt) 
   throws LayoutParseException
   {
     setAsciiArt(asciiArt);
   }
 
+  /**
+   * Default no argument constructor
+   */
   public AsciiArtGridBagLayout() {
   }
   
+  /**
+   * Get the drawing
+   * @return The string that contains the Ascii art
+   */
   public String getAsciiArt() {
     return asciiArt;
   }
 
+  /**
+   * Set the drawing
+   * @param asciiArt The Ascii art drawing to use
+   * @throws LayoutParseException If something goes wrong while parsing asciiArt
+   */
   public void setAsciiArt(String asciiArt) 
   throws LayoutParseException
   {
@@ -81,6 +103,11 @@ extends GridBagLayout
     }
   }
 
+  /**
+   * Establish the GridBagConstraints for the given named component and set them for the component
+   * @param componentName The name that is used in the layout to adress the component
+   * @param comp The component that will get its constraints applied to
+   */
   public void setConstraints(String componentName, Component comp) {
     GridBagConstraints cons = gblb.makeGridBagConstraints(componentName);
     super.setConstraints(comp, cons);
@@ -90,7 +117,7 @@ extends GridBagLayout
   }
   
   /**
-   * Set the constraints for all the components of the map
+   * Set the constraints for all the components of the map.
    * @param cm The component by name map
    */
   public void setConstraints(Map<String, Component> cm) {
@@ -99,32 +126,85 @@ extends GridBagLayout
     }
   }
   
+  /**
+   * Get the MaxX
+   * @return the MaxX
+   */
   public int getMaxX() { return gblb.maxx; }
   
+  /**
+   * Get the MaxY
+   * @return the MaxY
+   */
   public int getMaxY() { return gblb.maxy; }
   
+  /**
+   * Get the component for the name
+   * @param name The name
+   * @return the component for the name
+   */
   public Component getComponent(String name) { return componentsByName.get(name); }
 
+  /**
+   * get the constraints for the name
+   * @param name The name
+   * @return The constraints
+   */
   public GridBagConstraints getConstraints(String name) { return constraintsByName.get(name); }
   
+  /**
+   * Get all the components
+   * @return A list of all the components
+   */
   public List<Component> getAllComponents() { return new ArrayList<>(componentsByName.values()); }
   
+  /**
+   * Get all the component names
+   * @return A list of all the component names
+   */
+  public List<String> getAllComponentNames() { return new ArrayList<>(componentsByName.keySet()); }
+  
+  /**
+   * Get all the constraint names
+   * @return A list of all the constraint names
+   */
+  public List<String> getAllConstraintNames() { return new ArrayList<>(constraintsByName.keySet()); }
+  
+  /**
+   * Add all components the the container, from the list of components.
+   * Useful if you already have a list of component names.
+   * @param cr the container to add to
+   */
   public void addAllComponentsTo(Container cr) {
     for (String k : componentsByName.keySet()) {
       cr.add(componentsByName.get(k), constraintsByName.get(k));
     }
   }
   
+  /**
+   * Set the x weight for all the components in the list (comma-separated list, no whitespace) 
+   * @param rectNameList the list, example "a,x,y"
+   * @param val the x weight to set to
+   */
   public void setWeightx(String rectNameList, double val) {
     gblb.setWeightx(rectNameList, val); 
     resetConstraints(rectNameList);
   }
   
+  /**
+   * Set the y weight for all the components in the list (comma-separated list, no whitespace) 
+   * @param rectNameList the list, example "a,x,y"
+   * @param val the y weight to set to
+   */
   public void setWeighty(String rectNameList, double val) {
     gblb.setWeighty(rectNameList, val); 
     resetConstraints(rectNameList);
   }
   
+  /**
+   * Reset the constraints to what we have in this layout, to all the elements that are in the list  (comma-separated list, no whitespace)
+   * @param rectNameList the list, example "a,x,y"
+   */
   private void resetConstraints(String rectNameList) {
     String[] names = GridBagLayoutBuilder.split(rectNameList);
     for (String name : names) {
@@ -133,16 +213,49 @@ extends GridBagLayout
     }
   }
   
+  /**
+   * Set the x ipadding for all the components in the list (comma-separated list, no whitespace) 
+   * @param rectNameList the list, example "a,x,y"
+   * @param val the x ipadding to set to
+   */
   public void setIpadx(String rectNameList, int val) { 
     gblb.setIpadx(rectNameList, val);
     resetConstraints(rectNameList);
   }
   
+  /**
+   * Set the y ipadding for all the components in the list (comma-separated list, no whitespace) 
+   * @param rectNameList the list, example "a,x,y"
+   * @param val the y ipadding to set to
+   */
   public void setIpady(String rectNameList, int val) { 
     gblb.setIpady(rectNameList, val);
     resetConstraints(rectNameList);
   }
   
+  /**
+   * Return a list of all the CRects
+   * @return A list of all the CRects
+   */
   public List<CRect> getCRects() { return crects; }
+  
+  /**
+   * Return a list of all non-empty CRect names.
+   * This is useful if you already have a map, that contains components that uses the same names as the CRects (but also
+   * other names).
+   * A typical usage is in Groovy, where undeclared variables go into the "binding" :
+   * <pre>
+   * gbl.allCRectNames.each { gbl.setConstraints(it, binding[it]) }
+   * </pre>
+   * @return A list of names (never null, but can be empty)
+   */
+  public List<String> getAllCRectNames() {    
+    ArrayList<String> allNames = new ArrayList<>();
+    if (crects == null) return allNames;
+    for (CRect cr:crects) {
+      if (cr.name != null && cr.name.length() > 0) allNames.add(cr.name);
+    }
+    return allNames;
+  }
   
 }
